5365ac329bef0638c1852cac1d9de8a8f1f47dc3
harsh <harshranjansinha@outlook.in>
2021-08-26 02:27:35 +0530

    updated terminal system more to do

--- sql.cpp
+++ sql.cpp
@@ -24,6 +24,19 @@ public:
 	~sql() {}
 };
 
+class terminal : public sql
+{
+public:
+	terminal() { home(); }
+	void home();
+	void student();
+	void instructor();
+	void admin();
+	void display_record(string &, int &);
+	void update_record(){};
+	void insert_record(string &, int &);
+};
+
 void sql::update_student(int &id, string &details)
 {
 	int file_code = (id / 10000) + 100;
@@ -191,19 +204,237 @@ int sql::create_student_record(int &id, string &details)
 	student_id = (id - 100) * 10000 + student_id;
 	file.open(file_name, std::fstream::app);
 	file << "\n"
+		 << student_id << ","
 		 << details;
 	file.close();
 	return student_id;
 }
 
+void terminal::home()
+{
+	cout << "\n1. Student Login\n\n2. Instructor Login\n\n3. Admin Login\n\n";
+	int option;
+	cout << "Please select an option for login[example: 1] ";
+	cin >> option;
+	switch (option)
+	{
+	case 2:
+		system(clear.c_str());
+		instructor();
+		break;
+	case 3:
+		system(clear.c_str());
+		admin();
+		break;
+	default:
+		system(clear.c_str());
+		student();
+		break;
+	}
+}
+
+void terminal::student()
+{
+	string password, type = "student";
+	int id;
+	cout << "\nEnter Student ID:";
+	cin >> id;
+	cin.ignore();
+	cout << "\nEnter Password:";
+	getline(cin, password);
+	bool verify = check_id_password(id, password, type);
+	if (verify == false)
+		cout << "\nIncorrect ID or password\n";
+	else
+	{
+		system(clear.c_str());
+		display_record(type, id);
+	}
+	int pause_key = getchar();
+	system(clear.c_str());
+	home();
+}
+
+void terminal::instructor()
+{
+	string password, type = "instructor";
+	int id;
+	cout << "\nEnter Instructor ID: ";
+	cin >> id;
+	cin.ignore();
+	cout << "\nEnter Password: ";
+	getline(cin, password);
+	bool verify = check_id_password(id, password, type);
+	if (verify == false)
+		cout << "\nIncorrect ID or password\n";
+	else
+	{
+		system(clear.c_str());
+		cout << "1. Add Student Record\n\n2. Get Student Records\n\n Enter an option number[example: 1]: ";
+		int option;
+		cin >> option;
+		switch (option)
+		{
+		case 1:
+			type = "student";
+			system(clear.c_str());
+			insert_record(type, id);
+			break;
+		case 2:
+			type = "instructor";
+			system(clear.c_str());
+			display_record(type, id);
+			break;
+		default:
+			break;
+		}
+	}
+	cin.ignore();
+	int pause_key = getchar();
+	system(clear.c_str());
+	home();
+}
+
+void terminal::admin()
+{
+	cout << "1. Add Instructor\n\n2. Update Instructor\n\nEnter an option number[example: 1]: ";
+	int input;
+	cin >> input;
+	system(clear.c_str());
+	string type = "instructor";
+	if (input == 1)
+	{
+		fstream file("admin.csv", std::fstream::in);
+		int id;
+		file >> id;
+		file.close();
+		insert_record(type, id);
+	}
+	else if (input == 2)
+	{
+		cout << "Enter Instructor's id that is to be updated: ";
+		int id;
+		cin >> id;
+		id -= 100;
+		insert_record(type, id);
+	}
+	int pause_key = getchar();
+	system(clear.c_str());
+	home();
+}
+
+void terminal::display_record(string &type, int &id)
+{
+	if (type == "student")
+	{
+		int courses_id = (id / 10000) + 100;
+		vector<string> details = get_student_record(id);
+		vector<string> courses = get_courses(courses_id);
+		cout << "Student Name: " << details[3] << ", " << details[2] << "\n\n";
+		for (int i = 0; i < 2 * courses.size(); i++)
+		{
+			if (i < courses.size())
+				cout << "\t" << courses[i] << " |\t";
+			else if (i == courses.size())
+				cout << "\n\t" << setw(courses[i - 5].length()) << details[i - 1] << " |\t";
+			else
+				cout << "\t" << setw(courses[i - 5].length()) << details[i - 1] << " |\t";
+		}
+	}
+	else if (type == "instructor")
+	{
+		vector<string> instructor_details = get_instructor(id);
+		cout << "Instructor: " << instructor_details[3] << ", " << instructor_details[2] << "\n\n\tStudent Name |\t";
+		for (int i = 0; i < 5; i++)
+		{
+			cout << "\t" << instructor_details[i + 4] << " |\t";
+		}
+		string dashes(120,'-');
+		cout << "\n" << dashes;
+		id = (id - 100) * 10000 + 1;
+		vector<string> student_details = get_student_record(id);
+		while (true)
+		{
+			if (student_details[0] == "NULL")
+				break;
+			else
+			{
+				string name = student_details[2] + " " + student_details[3];
+				name = name.substr(0, 12);
+				cout << "\n\t" << name << " |\t";
+				for (int i = 0; i < 5; i++)
+				{
+					cout << "\t" << setw(instructor_details[i + 4].length()) << student_details[i + 4] << " |\t";
+				}
+				id++;
+				student_details = get_student_record(id);
+			}
+		}
+	}
+}
+
+void terminal::insert_record(string &type, int &id)
+{
+	if (type == "student")
+	{
+		cin.ignore();
+		string details, input;
+		vector<string> courses = get_courses(id);
+		cout << "\nEnter login password for student: ";
+		getline(cin, input);
+		details = input;
+		details.append(",");
+		cout << "\nEnter first name of student: ";
+		getline(cin, input);
+		details += input;
+		details.append(",");
+		cout << "\nEnter last name of student: ";
+		getline(cin, input);
+		details += input;
+		details.append(",");
+		for (int i = 0; i < courses.size(); i++)
+		{
+			cout << "\nEnter marks obtained in " << courses[i] << ": ";
+			getline(cin, input);
+			details += input;
+			details.append(",");
+		}
+		int generate_id = create_student_record(id, details);
+		system(clear.c_str());
+		cout << "Student Id is: " << generate_id << "\n";
+		display_record(type, generate_id);
+	}
+	else if (type == "instructor")
+	{
+		cin.ignore();
+		string details = to_string(id), input;
+		details.append(",");
+		cout << "Enter login password for instructor: ";
+		getline(cin, input);
+		details += input;
+		details.append(",");
+		cout << "\nEnter first name of instructor: ";
+		getline(cin, input);
+		details += input;
+		details.append(",");
+		cout << "\nEnter last name of instructor: ";
+		getline(cin, input);
+		details += input;
+		details.append(",");
+		for (int i = 0; i < 5; i++)
+		{
+			cout << "\nName of Subject " << i + 1 << ": ";
+			getline(cin, input);
+			details += input;
+			details.append(",");
+		}
+		create_file(id, details);
+		cout << "\nInstructor Id is: " << 100 + id << "\n";
+	}
+}
+
 int main()
 {
-
-	cout << "Hello";
-	sql test;
-	int x = 10001, y = 1;
-	string details = "10001,123-456,Mukta,Kumari,100,100,100,100,100,";
-	test.create_file(y, details);
-
+	terminal start;
 	return 0;
 }
